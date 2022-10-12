2edb2f050fa6dec9a875f3a90e150e79a893625c
Mohammad Imtiaz Shaikh <101206168+imtiazalisultan@users.noreply.github.com>
2022-10-12 20:08:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,8 @@ recognition.onresult=function(event){
 }
 
 function computerSpeech(words){
-    const speech=new SpeechSynthesisUtterance();
+    const speech=new SpeechSynthesisUtterance(); //The SpeechSynthesisUtterance interface of the Web Speech API represents a speech request. It contains the content the speech service should read and information about how to read it (e.g. language, pitch and volume.)
+
 
     speech.lang='en-US';
 
